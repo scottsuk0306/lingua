abb85742316604e05d8f87b6152fb786821a7795
scottsuk0306 <scottsuk0306@gmail.com>
2025-01-30 11:59:03 +0000

    Rename files

--- apps/mup/README.md
+++ apps/mup/README.md
@@ -4,6 +4,6 @@
 |-----------|-------------------|----------|
 | Output Scaling | Multiply the output of the embedding by `scale_emb` | [view code](transformer.py#L587)
 | Residual Connection Scaling | Scale the output tensor of a block before adding to each residual connection in each layer by `scale_depth/√num_layers` | [view code](transformer.py#L556)
-| Initialization of Tensors | Set the initialization standard deviation of each two-dimensional tensor parameter to `init_std/√(dm/dbase)`, and set other parameters' initialization to 0.1 |
+| Initialization of Tensors | Set the initialization standard deviation of each two-dimensional tensor parameter to `init_std/√(dm/dbase)`, and set other parameters' initialization to `init_std` |
 | Learning Rate Scaling of Tensors | Adjust the learning rate of each two-dimensional tensor parameter to `1/(dm/dbase)` times the learning rate of other parts (or the overall learning rate) |
 | LM Head Scaling | Adjust the output logits to `1/(dm/dbase)` times the original value |
--- apps/mup/test.py
+++ apps/mup/test.py
@@ -1,7 +1,7 @@
-from apps.mup.transformer import BaseTransformer, BaseTransformerArgs
+from apps.mup.blocks import BaseTransformer, BaseTransformerArgs
 
 
-if __name__ == "__main__":
+def test_init():
     args = BaseTransformerArgs(
         dim=256,
         n_layers=2,
@@ -10,3 +10,28 @@ if __name__ == "__main__":
     
     model = BaseTransformer(args)
     print(model)
+    
+    # BaseTransformer(
+    #   (rope_embeddings): RotaryEmbedding()
+    #   (layers): ModuleList(
+    #     (0-1): 2 x TransformerBlock(
+    #       (attention): Attention(
+    #         (wq): Linear(in_features=256, out_features=256, bias=False)
+    #         (wk): Linear(in_features=256, out_features=256, bias=False)
+    #         (wv): Linear(in_features=256, out_features=256, bias=False)
+    #         (wo): Linear(in_features=256, out_features=256, bias=False)
+    #       )
+    #       (feed_forward): FeedForward(
+    #         (w1): Linear(in_features=256, out_features=768, bias=False)
+    #         (w3): Linear(in_features=256, out_features=768, bias=False)
+    #         (w2): Linear(in_features=768, out_features=256, bias=False)
+    #       )
+    #       (attention_norm): RMSNorm()
+    #       (ffn_norm): RMSNorm()
+    #     )
+    #   )
+    # )
+    
+
+if __name__ == "__main__":
+    test_init()
